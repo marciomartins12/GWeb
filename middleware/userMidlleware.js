25fda80938b7bf4b89fb89133c1a79a5d1fabd85
marciomartins12 <mmarciom1martins635@gmail.com>
2024-09-15 16:34:16 -0300

    clean

--- middleware/userMidlleware.js
+++
@@ -1,8 +0,0 @@
-function userMiddleware(req, res, next) {
-    if (req.session.user && req.session.user.email) {
-        return next();
-    } else {
-        res.render("login", {mensagem : "precisa entrar com sua conta"});
-    }
-}
-module.exports = userMiddleware;
