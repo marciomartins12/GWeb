97116d950b5af373edd5217da322edda0c5f2091
marciomartins12 <mmarciom1martins635@gmail.com>
2024-09-15 16:59:12 -0300

    list

--- accountsNodejs/index.js
+++ accountsNodejs/index.js
@@ -1,3 +1,21 @@
-const inquire = require("inquirer");
-const fs = require("fs");
-console.log("teste")
+import inquirer from "inquirer";
+import fs from "fs";
+import chalk from "chalk";
+import { type } from "os";
+
+console.log(chalk.blue('iniciando code'));
+main();
+
+function main() {
+    console.clear()
+
+    inquirer.prompt({
+        type: "list",
+        message: "escolha uma opção:",
+        choices: ["criar conta", "ver saldo", "sacar", "depositar", "sair"]
+    }
+    ).then((resposta) => {
+        console.log(resposta[""])
+    }).catch(err => console.log(err))
+    // Código principal
+}
