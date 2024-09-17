a7e952bd87c7a4727c5b9d0ae19c9e98c8acee98
marciomartins12 <mmarciom1martins635@gmail.com>
2024-09-17 17:17:02 -0300

    config||

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const express = require("express");
 const app = express();
+const { engine } = require("express-handlebars");
 const Router = require("./routers/mainRouter");
+
+
+app.engine("handlebar", engine({
+    defaultLayout: "main", runtimeOptions: {
+        allowProtoPropertiesByDefault: true,
+    }
+}))
+app.set("view engine", "handlebras");
+
+app.engine("handlebars", engine({
+    defaultLayout: "main",
+    helpers: {
+        json: (context) => JSON.stringify(context)
+    },
+    runtimeOptions: {
+        allowProtoPropertiesByDefault: true,
+    }
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+    secret: 'ludovicensestudiositesenhasecreta',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false } 
+}));
+
 app.use(Router)
-
-
-
-
-
 app.listen(8080);
