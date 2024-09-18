babcd19ad33cf4bc173f298a28166ee1f469df2f
marciomartins12 <mmarciom1martins635@gmail.com>
2024-09-18 19:24:57 -0300

    login sssscfll

--- routers/mainRouter.js
+++ routers/mainRouter.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Router = express.Router();
+const userModel = require("../models/user");
 const userAuthenticate = require("../middlewares/userAuthenticate")
 
 Router.get("/", (req, res) => {
@@ -10,5 +11,31 @@ Router.get("/login", (req, res) => {
     res.render("login");
 })
 
+
+
+
+Router.post("/tryLogin", async(req, res)=>{
+    const {email, senha } = req.body;
+
+    try {
+        userModel.findOne({
+            where : {email : email, senha : senha}
+        }).then((conta)=>{
+            
+            req.session.user = {
+                id : conta.iduser,
+                email : conta.email,
+            }
+
+            res.redirect("/")
+        }).catch(err=>console.log(err))
+
+
+
+
+    } catch (error) {
+        
+    }
+})
 
 module.exports = Router
