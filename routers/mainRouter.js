87db5ec9222d5746f0cf47aee4c3dc963d796890
marciomartins12 <mmarciom1martins635@gmail.com>
2024-09-20 19:21:00 -0300

    init page new post

--- routers/mainRouter.js
+++ routers/mainRouter.js
@@ -1,19 +1,33 @@
 const express = require("express");
 const Router = express.Router();
 const userModel = require("../models/user");
+const postModel = require("../models/post")
 const userAuthenticate = require("../middlewares/userAuthenticate")
 
 Router.get("/",userAuthenticate ,(req, res) => {
+
     res.render("home");
-})
+});
 
 Router.get("/login", (req, res) => {
     res.render("login");
+});
+
+Router.get("/newPost", (req, res)=>{
+    res.render("newPost");
 })
 
 
 
 
+
+
+
+
+
+Router.post("enviandoNewPost", async(req,res)=>{
+await 
+})
 Router.post("/tryLogin", async(req, res)=>{
 
     const {email, senha } = req.body;
