8ed9f102c419225e5f0adb10e782aeefc16e1547
marciomartins12 <mmarciom1martins635@gmail.com>
2024-10-02 17:26:57 -0300

    view comments

--- routers/mainRouter.js
+++ routers/mainRouter.js
@@ -6,6 +6,7 @@ const userModel = require("../models/user");
 const postModel = require("../models/post");
 const likeModel = require("../models/like");
 const followerModel = require("../models/followers");
+const commentModel = require("../models/comment");
 const userAuthenticate = require("../middlewares/userAuthenticate");
 
 const uploadMultiple = upload.fields([
@@ -33,7 +34,7 @@ Router.get("/", userAuthenticate, async (req, res) => {
             d="M16.792 3.904A4.989 4.989 0 0 1 21.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 0 1 4.708-5.218 4.21 4.21 0 0 1 3.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 0 1 3.679-1.938m0-2a6.04 6.04 0 0 0-4.797 2.127 6.052 6.052 0 0 0-4.787-2.127A6.985 6.985 0 0 0 .5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 0 0 3.518 3.018 2 2 0 0 0 2.174 0 45.263 45.263 0 0 0 3.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 0 0-6.708-7.218Z">
         </path>
     </svg>` ;
-    
+
         return {
             ...post.dataValues,
             imagem_post: post.imagem_post ? `data:image/png;base64,${post.imagem_post.toString('base64')}` : null,
@@ -256,6 +257,23 @@ Router.post("/like/:id", userAuthenticate, async (req, res) => {
         post_id: idPost,
         user_id: user
     }).then(() => res.redirect(`/post/${idPost}`)).catch((er) => console.log(er))
+})
+
+Router.get("/verComentarios/:id", userAuthenticate, async (req, res) => {
+    const idPost = req.params.id;
+    const comentarios = await commentModel.findAll({ where: { post_id: idPost } });
+    if (comentarios) {
+        const comentariosFormatados = comentarios.map(async (comentario) => {
+            let usuarioComentou = await userAuthenticate.findByPk(comentario.user_id)
+            let imagemUsuario = usuarioComentou.foto_perfil ? `data:imagem/png;base64,${usuarioComentou.foto_perfil.toString("base64")}` : "/img/imagemPadrao.png"
+            return {
+                ...comentario.dataValues,
+                imagemUser: imagemUsuario,
+                userName: usuarioComentou.nome
+            }
+        });
+        res.render("viewComment", { comentariosFormatados});
+    }
 })
 
 module.exports = Router
