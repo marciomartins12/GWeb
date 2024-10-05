cf7bf59b88cfb23907209538d60207a2852ff781
marciomartins12 <mmarciom1martins635@gmail.com>
2024-10-05 15:16:14 -0300

    like

--- routers/mainRouter.js
+++ routers/mainRouter.js
@@ -18,7 +18,7 @@ const uploadMultiple = upload.fields([
 
 Router.get("/", userAuthenticate, async (req, res) => {
 
-    const posts = await postModel.findAll({order : [["idpost", "DESC"]]});
+    const posts = await postModel.findAll({ order: [["idpost", "DESC"]] });
     const usuarioLogado = await userModel.findByPk(req.session.user.id)
     const imagem = usuarioLogado.foto_perfil ? `data:imagem/png;base64,${usuarioLogado.foto_perfil.toString("base64")}` : "/img/imagemPadrao.png"
 
@@ -28,20 +28,13 @@ Router.get("/", userAuthenticate, async (req, res) => {
             where: { post_id: post.idpost }
         })
         let user = await userModel.findByPk(post.user_id)
-        const likeOrnot = await likeModel.findOne({ where: { post_id: post.idpost, user_id: req.session.user.id } });
-        const lkn = likeOrnot ? `<svg class="ponto descurtir ${post.idpost}"  aria-label="Descurtir" class="x1lliihq x1n2onr6 xxk16z8" fill="currentColor" height="24" role="img" viewBox="0 0 48 48" width="24"><title>Descurtir</title><path d="M34.6 3.1c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5s1.1-.2 1.6-.5c1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path></svg>` : `<svg class="ponto curtir ${post.idpost}"  aria-label="Curtir" class="x1lliihq x1n2onr6 xyb1xck" fill="currentColor" height="24"
-        role="img" viewBox="0 0 24 24" width="24">
-        <title>Curtir</title>
-        <path
-            d="M16.792 3.904A4.989 4.989 0 0 1 21.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 0 1 4.708-5.218 4.21 4.21 0 0 1 3.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 0 1 3.679-1.938m0-2a6.04 6.04 0 0 0-4.797 2.127 6.052 6.052 0 0 0-4.787-2.127A6.985 6.985 0 0 0 .5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 0 0 3.518 3.018 2 2 0 0 0 2.174 0 45.263 45.263 0 0 0 3.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 0 0-6.708-7.218Z">
-        </path>
-    </svg>` ;
-
+        const likeOrNot = await likeModel.findOne({ where: { post_id: post.idpost, user_id: req.session.user.id } });
+        
         return {
             ...post.dataValues,
             imagem_post: post.imagem_post ? `data:image/png;base64,${post.imagem_post.toString('base64')}` : null,
             likes: contadorCurtidas,
-            lkn: lkn,
+            likeOrNot,
             user_post: user.nome,
             userId: user.iduser,
             totalComentarios: totalComentarios == 0 ? "" : totalComentarios,
@@ -55,6 +48,23 @@ Router.get("/", userAuthenticate, async (req, res) => {
         res.render("home", { mensagem: "<p class='mesangemF'>Nenhuma postagem foi encontrada.</p>" });
     }
 
+});
+Router.post("/likeOrDeslike/:id", userAuthenticate, async (req, res) => {
+    const { likeOrNot } = req.body;
+    console.log(likeOrNot)
+    if (likeOrNot == "curtir") {
+        await likeModel.create({
+            post_id: req.params.id,
+            user_id: req.session.user.id
+        })
+        return res.json({ lknort: true });
+    }else {
+        await likeModel.destroy({where : {
+            post_id: req.params.id,
+            user_id: req.session.user.id
+        }})
+        return res.json({ lknort: false });
+    }
 });
 
 Router.get("/login", (req, res) => {
@@ -141,19 +151,13 @@ Router.get("/post/:id", userAuthenticate, async (req, res) => {
     if (post) {
         const user = await userModel.findByPk(post.user_id);
         const likeOrNot = await likeModel.findOne({ where: { post_id: req.params.id, user_id: req.session.user.id } });
-        const lkn = likeOrNot ? `<svg class="ponto descurtir"  aria-label="Descurtir" class="x1lliihq x1n2onr6 xxk16z8" fill="currentColor" height="24" role="img" viewBox="0 0 48 48" width="24"><title>Descurtir</title><path d="M34.6 3.1c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5s1.1-.2 1.6-.5c1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path></svg>` : `<svg class="ponto curtir" aria-label="Curtir" class="x1lliihq x1n2onr6 xyb1xck" fill="currentColor" height="24"
-       role="img" viewBox="0 0 24 24" width="24">
-       <title>Curtir</title>
-       <path
-           d="M16.792 3.904A4.989 4.989 0 0 1 21.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 0 1 4.708-5.218 4.21 4.21 0 0 1 3.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 0 1 3.679-1.938m0-2a6.04 6.04 0 0 0-4.797 2.127 6.052 6.052 0 0 0-4.787-2.127A6.985 6.985 0 0 0 .5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 0 0 3.518 3.018 2 2 0 0 0 2.174 0 45.263 45.263 0 0 0 3.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 0 0-6.708-7.218Z">
-       </path>
-   </svg>` ;
+
         const contadorCurtidas = await likeModel.count({ where: { post_id: post.idpost } })
         const postFormatado = {
             ...post.dataValues,
             imagem_post: `data:image/png;base64,${post.imagem_post.toString("base64")}`,
             likes: contadorCurtidas,
-            lkn: lkn,
+            likeOrNot,
             user_post: user.nome,
             idPost: req.params.id,
             idusuario: user.iduser,
@@ -244,24 +248,6 @@ Router.post("/follow/:id", userAuthenticate, async (req, res) => {
 
 });
 
-Router.post("/deslike/:id", userAuthenticate, async (req, res) => {
-    const idPost = req.params.id;
-    const user = req.session.user.id;
-
-    await likeModel.destroy({ where: { post_id: idPost, user_id: user } }).then(() => {
-        res.redirect(`/post/${idPost}`);
-    }).catch((er) => console.log(er))
-})
-
-Router.post("/like/:id", userAuthenticate, async (req, res) => {
-    const idPost = req.params.id;
-    const user = req.session.user.id;
-
-    await likeModel.create({
-        post_id: idPost,
-        user_id: user
-    }).then(() => res.redirect(`/post/${idPost}`)).catch((er) => console.log(er))
-})
 
 Router.get("/verComentarios/:id", userAuthenticate, async (req, res) => {
     const idPost = req.params.id;
@@ -303,7 +289,7 @@ Router.post("/enviarComment/:id", userAuthenticate, async (req, res) => {
     }
 })
 Router.get("/explorar", userAuthenticate, async (req, res) => {
-      const user = await userModel.findByPk(req.session.user.id);
+    const user = await userModel.findByPk(req.session.user.id);
     const imagem = user.foto_perfil ? `data:imagem/png;base64,${user.foto_perfil.toString("base64")}` : "/img/imagemPadrao.png"
     const post = await postModel.findAll({
         limit: 10,
@@ -317,7 +303,7 @@ Router.get("/explorar", userAuthenticate, async (req, res) => {
     })
 
 
-    res.render("explorar", { publicacoes, imagem})
+    res.render("explorar", { publicacoes, imagem })
 });
 
 
@@ -334,7 +320,7 @@ Router.get("/explorerUser", userAuthenticate, async (req, res) => {
         }
     });
     if (usuariosEncontrados) {
-       
+
         const usuarioFormatados = usuariosEncontrados.map((user) => {
 
             return {
